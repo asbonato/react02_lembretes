1a84bfe1280261df6defc80d3c13c8298d60964d
Sergio Bonato <asbonato@gmail.com>
2020-04-01 02:08:52 -0300

    refatoracao, touch e delecao

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import LembreteItem from './components/LembreteItem';
+import LembreteInput from './components/LembreteInput';
 
 export default function App() {
-  const [lembrete, setLembrete] = useState('');
   const [lembretes, setLembretes] = useState([]);
   const [contadorLembretes, setContadorLembretes] = useState(0);
 
-  //captura o texto digitado
-  const capturarLembrete = (lembrete) => {
-    setLembrete(lembrete)
+  //deletar lembrete
+  const removerLembrete = (keyASerRemovida) => {
+    setLembretes(lembretes => {
+      return lembretes.filter ((lembrete) => {
+        return lembrete.key !== keyASerRemovida
+      })
+    });
   };
 
   //para adicionar o que foi digitado
-  const adicionarLembrete = () => {
+  const adicionarLembrete = (lembrete) => {
     setLembretes(lembretes => {
       console.log(lembretes);
       setContadorLembretes(contadorLembretes + 1);
@@ -24,24 +29,16 @@ export default function App() {
   }
   return (
     <View style={styles.telaPrincipalView}>
-      <View style={styles.lembreteView}>
-        {/*O usuário irá inserir os lembretes aqui*/}
-        <TextInput placeholder="Lembrar..."
-          style={styles.lembreteInputText}
-          onChangeText={capturarLembrete}
-          value={lembrete}
-        />
-        <Button title="+"
-          onPress={adicionarLembrete}
-        />
-      </View>
+      <LembreteInput onAdicionarLembrete={adicionarLembrete}/>
+
       <FlatList
-        data={lembretes}/*coleção de lembretes*/
-        renderItem={ /*mapeamento*/
-          lembrete => (/*dado um lembrete gera uma view*/
-            <View style={styles.itemNaLista}>
-              <Text>{lembrete.item.value}</Text>
-            </View>
+        data={lembretes}
+        renderItem={ 
+          lembrete => (
+            <LembreteItem 
+            chave={lembrete.item.key}
+            lembrete={lembrete.item.value}
+            onDelete={removerLembrete}/>
           )
         }
       />
@@ -52,25 +49,5 @@ export default function App() {
 const styles = StyleSheet.create({
   telaPrincipalView: {
     padding: 50
-  },
-  lembreteView: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  lembreteInputText: {
-    width: '80%',
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    padding: 2,
-    marginBottom: 8
-  },
-  itemNaLista: {
-    padding: 12,
-    backgroundColor: '#CCC',
-    borderColor: '#000',
-    borderWidth: 1,
-    marginBottom: 8,
-    borderRadius: 8
   }
 });
